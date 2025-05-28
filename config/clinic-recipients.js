e3bff0576527411536d224774a27d068116ebc15
benjyalper <benjyalper@hotmail.com>
2025-05-28 22:19:53 +0300

    facelift

--- config/clinic-recipients.js
+++ config/clinic-recipients.js
@@ -4,8 +4,8 @@ export const clinicEmailRecipients = {
         'benjyalper@hotmail.com', 'bintyalper@gmail.com', 'benjyalper@hotmail.com', 'inbalkeshet111@hotmail.com', 'yaniv.shavit@gmail.com', 'morshani20@gmail.com', 'rnt832@gmail.com', 'Rachelmo1@clalit.org.il', 'Galiagoldkatz@gmail.com'
     ],
     clalit: [
-        'dalia@clalit.org',
-        'eyal@clalit.org'
+        'dalia@clalit.org'
+
     ],
     demo1: [
         'benjyalper@hotmail.com',
